011155e95cdc726f76fecff6149ff9621781ca38
Roberto Barna <soromesolo@gmail.com>
2025-04-07 23:15:17 +0200

    linting

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::env;
 
 fn main() {
-    let args:Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         println!("Immettere posizione da calcolare.");
-        return
+        return;
     }
 
     let take_at;
@@ -12,17 +12,21 @@ fn main() {
         Ok(r) => take_at = r,
         Err(_e) => {
             println!("Immettere un valore valido");
-            return
+            return;
         }
     }
 
-    let mut fib:Vec<u128> = Vec::from([0,1]);
+    let mut fib: Vec<u128> = Vec::from([0, 1]);
     calc_fib(take_at, &mut fib);
 
-    println!("Fibonacci alla posizione {} è: {}", take_at, fib.last().unwrap())
+    println!(
+        "Fibonacci alla posizione {} è: {}",
+        take_at,
+        fib.last().unwrap()
+    )
 }
 
-fn calc_fib(target:usize, fib: &mut Vec<u128>) {
+fn calc_fib(target: usize, fib: &mut Vec<u128>) {
     while fib.len() <= target {
         let next = fib.last().unwrap() + fib.get(fib.len() - 2).unwrap();
         fib.push(next);
